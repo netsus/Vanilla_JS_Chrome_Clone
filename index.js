06291bc371574c93ab14e2f813b174bd1a648037
netsus <57648890+netsus@users.noreply.github.com>
2020-12-19 17:49:28 +0900

    #1.6 Furst JS Variable
    Useing Variable
    1) Create
    2) Initialize
    3) Use it
    -> When 1), 2), using 'let'

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Im Working. Im JS. Im Beautiful. Im worth it")
+// Using Variable
+// Create
+// Initialize
+// Use
+
+let a = 221; // Create & Initializs
+let b = a - 5;  // Use
+a = 4; // Update
+console.log(b, a);
