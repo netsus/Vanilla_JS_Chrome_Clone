89e52e1e6e4dbedbccd3997fc1c49ebad4513118
netsus <57648890+netsus@users.noreply.github.com>
2020-12-19 18:04:39 +0900

    #1.8 Data Type
    Multiline Comments
    String
    Boolean (true or false)
    Number
    Float

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-// Using Variable
-// Create
-// Initialize
-// Use
+/*
+Using Variable
+Create
+Initialize
+Use
+*/
+// Mulitiline Comments
 
-const a = 221; // Const는 상수
-let b = a - 5;  // Use
-a = 4; // Update
-console.log(b, a);
+
+// String
+const what = "Niclolas";
+console.log(what);
+
+// Boolean
+const what2 = false;
+const wat2 = true;
+console.log(what2,wat2);
+
+//Number
+const what3 = 110;
+console.log(what3);
+
+//Float
+const wat4 = 55.1;
+console.log(wat4);
