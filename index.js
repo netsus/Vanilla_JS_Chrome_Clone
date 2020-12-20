fb927f773814396fa8dff36650e34adafa64b848
netsus <57648890+netsus@users.noreply.github.com>
2020-12-20 23:16:38 +0900

    #2.4
    addEventListener의
    두번쨰 인자로 받는 function에는 event가 기본적으로 전달된다.

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 // 브라우저에서 제공하는 함수 존재
 // -> HTML을 다루는게 목적
 
-const title = document.getElementById("title");
-// index.html 페이지의 개발자도구의 Console에 아래 내용이 출력된다.
-console.log(title);
+const title = document.querySelector("#title");
 
-console.error("Fuck");
+function handleResiae(event){
+    console.log(event);
+    // console.log("I have been resized");
+}
+
+window.addEventListener("resize", handleResiae);
 
 
-//DOM
+function handleClick(){
+    title.style.color = 'yellow';
+}
 
-title.innerHTML = "Hi! From JS";
-
-// title의 모든 오브젝트를 보여줌 -> Console에서
-console.dir(title);
-
-// title의 style 변경 가능
-title.style.color = "yellow";
-
-// HTML과 CSS 문서 전체 오브젝트 보여줌.
-console.dir(document);
-
-document.title = "I own you now"
-
-// querySelector는 CSS 선택자와 유사
-// 제일 많이 사용!!
-const qeurySelector = document.querySelector("#title")
+title.addEventListener("click",handleClick);
