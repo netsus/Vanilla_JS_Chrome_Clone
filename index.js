359f2d18f9c6599ffde35d5b5545f375123f571a
netsus <57648890+netsus@users.noreply.github.com>
2020-12-28 23:56:43 +0900

    #2.7 Function practice Two
    title.classList.contains();
    title.classList.remove();
    title.classList.add();
    -> title.classList.toggle()
    :When contains classList -> remove, and when not in classList -> add
    That was a toggle

--- index.js
+++ index.js
@@ -3,32 +3,19 @@
 
 const title = document.querySelector("#title");
 
-const BASE_COLOR = 'white';
-const OTHER_COLOR = '#7f8c8d'
+const CLICKED_CLASS = 'clicked';
 
-function handleClick() {
-    const currentColor = title.style.color;
-    if (currentColor  === BASE_COLOR){
-        title.style.color = OTHER_COLOR;
-    } else {
-        title.style.color = BASE_COLOR;
-    }
+function handleClick(){
+    title.classList.toggle(CLICKED_CLASS);
+    // const hasClass = title.classList.contains(CLICKED_CLASS);
+    // if(hasClass){
+    //     title.classList.remove(CLICKED_CLASS);
+    // } else {
+    //     title.classList.add(CLICKED_CLASS);
+    // }
 }
-
 function init(){
-    title.style.color = BASE_COLOR;
     title.addEventListener('click',handleClick)
 }
 
 init();
-
-function handleOffline(){
-    console.log("Bye Bye");
-}
-
-function handleOnline(){
-    console.log("Welcome back");
-}
-
-window.addEventListener("offline",handleOffline)
-window.addEventListener("online",handleOnline)
