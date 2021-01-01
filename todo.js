e11654bdfe5884993eca9cf8d31278791745ea55
netsus <57648890+netsus@users.noreply.github.com>
2021-01-01 21:56:34 +0900

    #3.4 Todo List Part One
    document.createElement()
    appendHild()

---
+++ todo.js
@@ -0,0 +1,40 @@
+const toDoform = document.querySelector(".js-toDoForm"),
+toDoInput = toDoform.querySelector("input"),
+toDoList = document.querySelector(".js-toDoList");
+
+
+const TODOS_LS = 'toDos';
+
+function paintToDo(text){
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.innerHTML = "X";
+    const span = document.createElement("span");
+    span.innerText = text;
+    li.appendChild(delBtn);
+    li.appendChild(span);
+    
+    toDoList.appendChild(li);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
+}
+
+
+function loadToDos(){
+    const toDos = localStorage.getItem(TODOS_LS);
+    if(toDos !== null){
+
+    }
+}
+
+function init(){
+    loadToDos();
+    toDoform.addEventListener("submit",handleSubmit);
+}
+
+init();
