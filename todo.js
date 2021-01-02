df12916d182ac6f674a1371b3f5be441593d96b3
netsus <57648890+netsus@users.noreply.github.com>
2021-01-02 20:58:58 +0900

    #3.6 Make ToDo List part three
    1. filter function
    2. forEach
    
    parseInt, removeChild, event.target, parentNode

--- todo.js
+++ todo.js
@@ -3,7 +3,21 @@ toDoInput = toDoForm.querySelector('input'),
 toDoList = document.querySelector('.js-toDoList');
 
 const TODOS_LS = 'TODO';
-const toDos = [];
+let toDos = [];
+
+
+
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
+
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -14,7 +28,7 @@ function paintToDo(text){
     const delBtn = document.createElement('button');
     const span = document.createElement('span');
     const newId = toDos.length + 1;
-    delBtn.innerHTML = 'X ';
+    delBtn.innerHTML = 'X';
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
@@ -24,6 +38,7 @@ function paintToDo(text){
         text: text,
         id: newId
     };
+    delBtn.addEventListener('click',deleteToDo);
     toDos.push(toDoObj);
     saveToDos();
 }
