d9329c51a68db3460b4917a24031b393b9f34c69
netsus <57648890+netsus@users.noreply.github.com>
2021-01-22 22:40:19 +0900

    assignment9 fin
    assignment9 fin

--- assignment/assignment9.js
+++ assignment/assignment9.js
@@ -8,12 +8,13 @@ const divide = document.querySelector('.divide');
 const keys = document.querySelector('.calculator__numbers');
 const operator = document.querySelector('.calculator__oprator');
 
-let X = [];
-let Y = [];
-
+let exp = "";
+let num = "";
 
 function Reset(event){
     result.innerHTML=0;
+    exp="";
+    num="";
 }
 
 function Calculate(event){
@@ -26,13 +27,38 @@ function init() {
     keys.addEventListener('click', event => {
         if (event.target.matches('button')) {
             const key = event.target
-            console.log(key.innerText);
+            if (key.innerText==='='){
+                try {
+                    console.log(eval(exp));
+                    result.innerHTML = eval(exp);
+                    num="";
+                } catch {
+                    result.innerHTML = "Try again";
+                }
+            }
+            else {
+                exp+=key.innerText;
+                num+=key.innerText;
+                result.innerHTML=num;
+            }
+            console.log(exp);
         }
     });
+
     operator.addEventListener('click', event => {
         if (event.target.matches('button')) {
             const op = event.target
-            console.log(op.innerText);
+            if (op.innerText !== 'C'){
+                try {
+                    num="";
+                    result.innerHTML = eval(exp);
+                    exp+=op.innerText;                    
+                } catch  {
+                    num="";
+                }
+                
+            }
+            console.log(exp);
         }
     });
 
